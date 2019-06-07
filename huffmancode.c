fd24c804386927f7b9fcdd0132dbd24b9466cc40
Zara Veda <zaraveda.zv@gmail.com>
2019-06-07 15:52:45 +0700

    Add files via upload

---
+++ huffmancode.c
@@ -0,0 +1,10 @@
+/*
+ * Nama File	: huffmancode.c
+ * Deskripsi	: implementasi dari ADT huffman code (huffmancode.h)
+ * Author/NIM	: Nadia Melyani / 181524022
+ 				  Zara Veda Mahendra / 181524031
+ * Tanggal/Ver	: 07/06/2019 ver.0
+ * Compiler		: TDM-GCC 4.9.2
+**/
+
+#include "huffmancode.h"
---
+++ huffmancode.h
@@ -0,0 +1,16 @@
+/*
+ * Nama File	: huffmancode.h
+ * Deskripsi	: ADT huffman code
+ * Author/NIM	: Nadia Melyani / 181524022
+ 				  Zara Veda Mahendra / 181524031
+ * Tanggal/Ver	: 07/06/2019 ver.0
+ * Compiler		: TDM-GCC 4.9.2
+**/
+
+#ifndef huffmancode_h
+
+#define huffmancode_h
+
+
+
+#endif
