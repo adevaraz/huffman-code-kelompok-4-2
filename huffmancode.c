3ccb655e807faca2d6fb100c9c41c856cfc2a6d6
adevaraz <zaraveda.zv@gmail.com>
2019-06-08 15:27:07 +0700

    add constructor, validate, and sort

--- huffmancode.c
+++ huffmancode.c
@@ -10,4 +10,67 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "huffmancode.h"
+
+/*************** Constructor ***************/
+/**
+ * Membuat tree baru yang kosong.
+ * Mengembalikan sebuah tree dengan dengan root == NULL.
+ **/
+huffman_tree CreateEmptyTree() {
+	huffman_tree T;
+	
+	T.tree = NULL;
+	
+	return T;
+}
+
+/**
+ * Membuat tree baru yang sudah berisi satu Node sebagai root.
+ * Mengembalikan sebuah tree dengan dengan root == node.
+ **/
+huffman_tree CreateTree(huffman_node node) {
+	huffman_tree T = CreateEmptyTree();
+	
+	T.tree = node;
+	
+	return T;
+}
+
+/**
+ * Mengalokasikan node baru.
+ * Mengembalikan sebuah node dengan node.symbol = value,
+ * node.probability = prob dan node.left_c == node.right_c == node.parent == NULL.
+ * Jika alokasi gagal, modul mengembalikan NULL.
+ **/
+addr_huffman Allocate(infotype value, double prob) {
+	addr_huffman n_node = (addr_huffman) malloc(sizeof(huffman_node));
+	
+	if(n_node != NULL) {
+		n_node.symbol = value;
+		n_node.probability = prob;
+		n_node.left_c = NULL;
+		n_node.right_c = NULL;
+		n_node.parent = NULL;
+	}
+	
+	return n_node;
+}
+
+/*************** Validate ***************/
+/**
+ * Memeriksa apakah suatu node merupakan left_child dari
+ * parentnya atau bukan.
+ * Mengembalikan nilai .true. jika node merupakan left_child
+ * dari parentnya;
+ * Mengembalikan nilai .false. jika node bukan left_child
+ * dari parentnya.
+ **/
+bool IsLeftChild(huffman_node node) {
+	return (node == node.parent->left_c);
+}
+
+/************ Huffman Operations ************/
+
+huffman_tree SortProbability(huffman_tree *the_tree);
--- huffmancode.h
+++ huffmancode.h
@@ -9,16 +9,18 @@
 
 #include <stdio.h>
 #include <malloc.h>
+#include <stdbool.h>
 
 #ifndef huffmancode_h
 
 #define huffmancode_h
 
-typedef struct char infotype;
+typedef char infotype;
 
 typedef struct huffmanNode *addr_huffman;
 typedef struct huffmanNode  {
 	infotype symbol;
+	addr_huffman parent;
 	addr_huffman left_c;
 	addr_huffman right_c;
 	double probability;
@@ -27,6 +29,17 @@ typedef struct huffmanNode  {
 typedef struct {
 	addr_huffman tree;
 } huffman_tree;
+
+/*************** Constructor ***************/
+huffman_tree CreateEmptyTree();
+huffman_tree CreateTree(huffman_tree *the_tree, huffman_node the_root);
+addr_huffman Allocate(infotype value);
+
+/*************** Validate ***************/
+bool IsLeftChild(huffman_node node);
+
+/************ Huffman Operations ************/
+huffman_tree SortProbability(huffman_tree *the_tree);
 
 
 #endif
