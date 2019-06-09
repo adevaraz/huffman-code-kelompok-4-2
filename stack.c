cd6e1bf8b05196babf5e94c03cada9abdaf526af
nadiamelyani <nadia.melyani.tif418@gmail.com>
2019-06-09 14:19:19 +0700

    Create stack.c

---
+++ stack.c
@@ -0,0 +1,70 @@
+/*
+ * Nama File	: stack.c
+ * Deskripsi	: Implementasi dari stack.h
+ * Author/NIM	: Nadia Melyani / 181524022
+ 				  Zara Veda Mahendra / 181524031
+ * Tanggal/Ver	: 09/06/2019 ver.0
+ * Compiler		: TDM-GCC 4.9.2
+**/
+
+#include "stack.h"
+
+/**
+ * Membuat stack yang kosong
+ **/
+addr_stack CreateStack(stack *S) {
+	(*S).top = NULL;
+}
+
+/**
+ * Mengalokasikan tempat di memori untuk sebuah node baru
+ **/
+addr_stack Alloc(vartype info) {
+	addr_stack new_node = (addr_stack)malloc(sizeof(elmt_stack));
+	new_node->info = info;
+	new_node->next = NULL;
+}
+
+/**
+ * Menghapus keberadaan node di memori
+ **/
+addr_stack Deallocate(addr_stack *S) {
+	(*S)->next = NULL;
+	free(*S);
+}
+
+/**
+ * Menambahkan isi stack
+ **/
+void Push(stack *S, vartype info) {
+	addr_stack node = Alloc(info);
+	node->next = (*S).top;
+	(*S).top = node;
+}
+
+/**
+ * Mengambil isi stack
+ **/
+vartype Pop(stack *S) {
+	vartype pop;
+	addr_stack phelp;
+	pop = (*S).top->info;
+	phelp = (*S).top;
+	(*S).top = (*S).top->next;
+	Deallocate(&phelp);
+	
+	return pop;
+}
+
+/**
+ * Menghapus semua isi stack
+ **/
+void DeleteStack(stack *S) {
+	addr_stack pdel;
+	pdel = (*S).top;
+	while((*S).top != NULL) {
+		pdel = (*S).top;
+		(*S).top= (*S).top->next;
+		Deallocate(&pdel);
+	}
+}
