f5d39130d5843283b1915b2b2c82dd98e311b5a7
nadiamelyani <nadia.melyani.tif418@gmail.com>
2019-06-15 20:34:52 +0700

    Update sortedlist.h

--- sortedlist.h
+++ sortedlist.h
@@ -13,12 +13,13 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include "strlist.h"
 #include "boolean.h"
 
 typedef char infotype;
 
 struct huffmanNode;
-
 typedef struct elmt_list_t *addr_sorted;
 typedef struct elmt_list_t {
 	struct huffmanNode *info;
@@ -40,6 +41,9 @@ addr_sorted SearchNode(sorted_list L, infotype value);
 addr_sorted SearchNodeBefore(sorted_list L, addr_sorted node);
 boolean IsFirstElmt(sorted_list L, addr_sorted node);
 struct huffmanNode* GetFirstElmt(sorted_list L);
+void GenerateSortedList(List string_l, sorted_list *sorted_l, double total);
+double CountProbability(double freq, double count);
+void ClearArray(char *arr);
 
 /*************** Destructor ***************/
 addr_sorted DeallocateElmt(addr_sorted *N);
