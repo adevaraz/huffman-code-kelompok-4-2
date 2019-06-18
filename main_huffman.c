0d54f09e50f31a0f80141535748c5658078d9a45
nadiamelyani <nadia.melyani.tif418@gmail.com>
2019-06-18 21:32:25 +0700

    Update main_huffman.c

--- main_huffman.c
+++ main_huffman.c
@@ -7,10 +7,14 @@
 #include "strlist.h"
 #include "codelist.h"
 
+#define CODE_EXIST true
+#define CODE_NOT_EXIST false
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	char text[100] = { "kkennaalanbyaa" }, temp[26];
+	char text[100] = { "ananda bayu" }, temp[26];
+	char *converted_code;
 	int i, j, len, spaces = 0;
 	sorted_list node_list;
 	huffman_tree the_tree;
@@ -47,7 +51,14 @@ int main() {
 	printf("Str List : ");
 	PrintInfo(sentence);
 	
-	GenerateSortedList(sentence, &node_list, (double) (len - spaces));
+	InitFromProb(&node_list, 'a', 0.18);
+	InitFromProb(&node_list, 'e', 0.04);
+	InitFromProb(&node_list, 'k', 0.3);
+	InitFromProb(&node_list, 'l', 0.03);
+	InitFromProb(&node_list, 'n', 0.2);
+	InitFromProb(&node_list, 'b', 0.1);
+	InitFromProb(&node_list, 'y', 0.15);
+	//GenerateSortedList(sentence, &node_list, (double) (len - spaces));
 	
 	printf("\nSorted List :\n");
 	PrintSorted(node_list);
@@ -63,10 +74,17 @@ int main() {
 	codes = CreateHuffmanCode(the_tree);
 	PrintInfoCode(codes);
 	
-//	ConvertToSymbol(the_tree, "000110010100111110010111");
+	switch(ConvertToSymbol(the_tree, "00010111", &converted_code)) {
+		case CODE_EXIST :
+			printf("%s", &converted_code);
+			break;
+		case CODE_NOT_EXIST :
+			printf("The code can't be converted");
+			break;
+	}
 	
 	printf("\n");
-	ConvertToHuffmanCode(codes, sentence);
+//	ConvertToHuffmanCode(codes, sentence);
 	
 	printf("\nyahooo!");
 	getch();
